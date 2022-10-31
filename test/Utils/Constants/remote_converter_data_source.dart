5e016355d24d151b9d2993ecdb66ca936b2e5ffc
Mohamed Saeed <mhamad.saeed2015.ms@gmail.com>
2022-10-31 11:56:34 +0200

    Adding another testing group which test all test case for getAllCountriesCurrencies methods belong to remote_converter_data_source.dart.

--- test/Utils/Constants/remote_converter_data_source.dart
+++ test/Utils/Constants/remote_converter_data_source.dart
@@ -1,4 +1,5 @@
 import 'package:calculator_converter/Cores/network/error_model.dart';
+import 'package:calculator_converter/Features/currency_converter/data/models/conversions_model.dart';
 import 'package:calculator_converter/Features/currency_converter/data/models/country_currency_model.dart';
 import 'package:calculator_converter/Features/currency_converter/data/models/currency_model.dart';
 
@@ -21,6 +22,12 @@ class RemoteConverterDataSourceConstants {
       name: 'Anguilla',
     ),
   });
+
+  static const conversions = CurrencyConversionModel(
+      countriesIds: ['USD_EGP', 'EGP_USD'],
+      conversionsValues: [5.169343, 0.041397]);
+
+  static const conversionsMap={"USD_EGP": 5.169343, "EGP_USD": 0.041397};
 
   static const errorModel =
       ErrorModel(statusCode: 400, errorMessage: 'an error occurred');
--- test/features/currency_converter/data/data_sources/remote_converter_data_source_test.dart
+++ test/features/currency_converter/data/data_sources/remote_converter_data_source_test.dart
@@ -4,6 +4,7 @@ import 'package:calculator_converter/Cores/network/dio_helper.dart';
 import 'package:calculator_converter/Cores/network/error_model.dart';
 import 'package:calculator_converter/Cores/network/exception_mapper.dart';
 import 'package:calculator_converter/Features/currency_converter/data/data_source/remote_converter_data_source.dart';
+import 'package:calculator_converter/Features/currency_converter/data/models/conversions_model.dart';
 import 'package:calculator_converter/Features/currency_converter/data/models/country_currency_model.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -23,7 +24,7 @@ void main() {
   });
 
   group(
-      'Checking that all functionality of remote converter data source is working as expected',
+      'Checking that all functionality of  getAllCountriesCurrencies is working as expected',
       () {
     test(
         ' Testing that getAllCountriesCurrencies method return all countries currencies with status code 200',
@@ -59,7 +60,8 @@ void main() {
 
       ErrorModel errorModel = RemoteConverterDataSourceConstants.errorModel;
       final mapResult = errorModel.toJson();
-      final expectedResult = throwsA(predicate((model) => model is RemoteServerException));
+      final expectedResult =
+          throwsA(predicate((model) => model is RemoteServerException));
       when(mockDioHelper
           .getData(url: ApiEndPoints.allCountriesCurrenciesEndPoint, query: {
         'apiKey': ApiConstants.apiKey,
@@ -73,7 +75,76 @@ void main() {
         )),
       );
       //Act
-     result() async => await remoteConverterDataSource.getAllCountriesCurrencies();
+      result() async =>
+          await remoteConverterDataSource.getAllCountriesCurrencies();
+
+      //Assert
+      expect(result, expectedResult);
+    });
+  });
+
+  group(
+      'Checking that all functionality of getCurrenciesConversions is working as expected',
+      () {
+    const String fromCurrency = 'USD';
+    const String toCurrency = 'EGP';
+    test(
+        ' Testing that getCurrenciesConversions method return all countries currencies with status code 200',
+        () async {
+      //Arrange
+      CurrencyConversionModel conversions =
+          RemoteConverterDataSourceConstants.conversions;
+      const mapResult = RemoteConverterDataSourceConstants.conversionsMap;
+
+      when(mockDioHelper
+          .getData(url: ApiEndPoints.convertCurrenciesEndPoint, query: {
+        'apiKey': ApiConstants.apiKey,
+        'compact': 'ultra',
+        'q': '${fromCurrency}_$toCurrency,${toCurrency}_$fromCurrency'
+      })).thenAnswer(
+        (realInvocation) => Future.value(Response(
+          requestOptions: RequestOptions(
+            path: ApiConstants.baseUrl,
+          ),
+          data: mapResult,
+          statusCode: 200,
+        )),
+      );
+      //Act
+      final result = await remoteConverterDataSource.getCurrenciesConversions(
+          fromCurrency: fromCurrency, toCurrency: toCurrency);
+
+      //Assert
+      expect(result, conversions);
+    });
+
+    test(
+        ' Testing that getCurrenciesConversions method throw RemoteServerException when statusCode is not 200',
+        () async {
+      //Arrange
+
+      ErrorModel errorModel = RemoteConverterDataSourceConstants.errorModel;
+      final mapResult = errorModel.toJson();
+      final expectedResult =
+          throwsA(predicate((model) => model is RemoteServerException));
+      when(mockDioHelper
+          .getData(url: ApiEndPoints.convertCurrenciesEndPoint, query: {
+        'apiKey': ApiConstants.apiKey,
+        'compact': 'ultra',
+        'q': '${fromCurrency}_$toCurrency,${toCurrency}_$fromCurrency'
+      })).thenAnswer(
+        (realInvocation) => Future.value(Response(
+          requestOptions: RequestOptions(
+            path: ApiConstants.baseUrl,
+          ),
+          statusCode: 400,
+          data: mapResult,
+        )),
+      );
+      //Act
+      result() async =>
+          await remoteConverterDataSource.getCurrenciesConversions(
+              fromCurrency: fromCurrency, toCurrency: toCurrency);
 
       //Assert
       expect(result, expectedResult);
