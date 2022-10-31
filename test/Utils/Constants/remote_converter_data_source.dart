8fbb5222ea0f90bd0608b4a62ffafb567dd269e2
Mohamed Saeed <mhamad.saeed2015.ms@gmail.com>
2022-10-31 16:56:07 +0200

    Testing all functions with all success and failure test cases of converter repository class.

--- test/Utils/Constants/remote_converter_data_source.dart
+++ test/Utils/Constants/remote_converter_data_source.dart
@@ -1,30 +1,12 @@
 import 'package:calculator_converter/Cores/network/error_model.dart';
 import 'package:calculator_converter/Cores/network/exception_mapper.dart';
+import 'package:calculator_converter/Cores/network/general_failure_model.dart';
 import 'package:calculator_converter/Features/currency_converter/data/models/conversions_model.dart';
 import 'package:calculator_converter/Features/currency_converter/data/models/country_currency_model.dart';
 import 'package:calculator_converter/Features/currency_converter/data/models/currency_model.dart';
-import 'package:calculator_converter/Features/currency_converter/domain/entities/country_currency.dart';
 
 class RemoteConverterDataSourceConstants {
   static const currenciesWithId = CountryCurrencyModel(results: {
-    'AF': CurrencyModel(
-      alpha3: 'AFG',
-      currencyId: 'AFN',
-      currencyName: 'Afghan afghani',
-      currencySymbol: '؋',
-      id: 'AF',
-      name: 'Afghanistan',
-    ),
-    'AI': CurrencyModel(
-      alpha3: 'AIA',
-      currencyId: 'XCD',
-      currencyName: 'East Caribbean dollar',
-      currencySymbol: '\$',
-      id: 'AI',
-      name: 'Anguilla',
-    ),
-  });
-  static const currencies = CountryCurrency(results: {
     'AF': CurrencyModel(
       alpha3: 'AFG',
       currencyId: 'AFN',
@@ -50,6 +32,9 @@ class RemoteConverterDataSourceConstants {
   static const errorModel =
       ErrorModel(statusCode: 400, errorMessage: 'an error occurred');
 
-  static const remoteServerErrorModel =
+  static const remoteServerExceptionModel =
       RemoteServerException(errorModel: errorModel);
+  static const remoteServerFailureModel =
+  RemoteServerFailure('an error occurred');
+
 }
--- test/features/currency_converter/data/repository/converter_repository_test.dart
+++ test/features/currency_converter/data/repository/converter_repository_test.dart
@@ -1,7 +1,12 @@
+import 'package:calculator_converter/Cores/network/exception_mapper.dart';
+import 'package:calculator_converter/Cores/network/general_failure_model.dart';
 import 'package:calculator_converter/Features/currency_converter/data/data_source/remote_converter_data_source.dart';
+import 'package:calculator_converter/Features/currency_converter/data/models/conversions_model.dart';
 import 'package:calculator_converter/Features/currency_converter/data/models/country_currency_model.dart';
 import 'package:calculator_converter/Features/currency_converter/data/repository/converter_repository.dart';
+import 'package:calculator_converter/Features/currency_converter/domain/entities/conversions.dart';
 import 'package:calculator_converter/Features/currency_converter/domain/entities/country_currency.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -40,5 +45,71 @@ void main() {
       expect(rightResult, currencies);
     });
 
+    test(
+        ' Testing that getAllCountriesCurrencies method return throw RemoteServerException when statusCode is not 200',
+        () async {
+      //Arrange
+      RemoteServerException remoteServerException =
+          RemoteConverterDataSourceConstants.remoteServerExceptionModel;
+      when(mockBaseRemoteConverterDataSource.getAllCountriesCurrencies())
+          .thenThrow(remoteServerException);
+      RemoteServerFailure remoteServerFailure =
+          RemoteConverterDataSourceConstants.remoteServerFailureModel;
+      //Act
+      final result = await converterRepository.getAllCountriesCurrencies();
+      late RemoteServerFailure leftResult;
+      result.fold((l) {
+        leftResult = l as RemoteServerFailure;
+      }, (r) {});
+
+      //Assert
+      expect(result, Left(remoteServerFailure));
+    });
+  });
+
+  group(
+      'Checking that all functionality of  getCurrenciesConversions is working as expected',
+      () {
+    const String fromCurrency = 'USD';
+    const String toCurrency = 'EGP';
+    test(
+        ' Testing that getCurrenciesConversions method return all currencies conversions with status code 200',
+        () async {
+      //Arrange
+      CurrencyConversionModel conversions =
+          RemoteConverterDataSourceConstants.conversions;
+      when(mockBaseRemoteConverterDataSource.getCurrenciesConversions(
+              fromCurrency: fromCurrency, toCurrency: toCurrency))
+          .thenAnswer((_) => Future.value(conversions));
+      //Act
+      final result = await converterRepository.getCurrenciesConversions(fromCurrency: fromCurrency, toCurrency: toCurrency);
+      late CurrencyConversion rightResult;
+      result.fold((l) {}, (r) {
+        rightResult = r;
+      });
+      //Assert
+      expect(rightResult, conversions);
+    });
+
+    test(
+        ' Testing that getCurrenciesConversions method return throw RemoteServerException when statusCode is not 200',
+        () async {
+      //Arrange
+      RemoteServerException remoteServerException =
+          RemoteConverterDataSourceConstants.remoteServerExceptionModel;
+      when(mockBaseRemoteConverterDataSource.getCurrenciesConversions(fromCurrency: fromCurrency, toCurrency: toCurrency))
+          .thenThrow(remoteServerException);
+      RemoteServerFailure remoteServerFailure =
+          RemoteConverterDataSourceConstants.remoteServerFailureModel;
+      //Act
+      final result = await converterRepository.getCurrenciesConversions(fromCurrency: fromCurrency, toCurrency: toCurrency);
+      late RemoteServerFailure leftResult;
+      result.fold((l) {
+        leftResult = l as RemoteServerFailure;
+      }, (r) {});
+
+      //Assert
+      expect(result, Left(remoteServerFailure));
+    });
   });
 }
